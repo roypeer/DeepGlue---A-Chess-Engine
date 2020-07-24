7e4beb8bf5b4117481e14103e7cd4e2c1aedc4ca
Roy Peer <roypeer11@gmail.com>
2020-07-24 17:13:16 +0300

    Removed trackers, cleaned code

---
+++ chess_auxiliary_functions.py
@@ -0,0 +1,16 @@
+PAWN_VALUE = 1
+ROOK_VALUE = 5
+KNIGHT_VALUE = 3
+BISHOP_VALUE = 3
+QUEEN_VALUE = 9
+
+letter_to_value_dict = {'P': PAWN_VALUE, 'R': ROOK_VALUE, 'N': KNIGHT_VALUE, 'B': BISHOP_VALUE, 'Q': QUEEN_VALUE,
+                        'p': -1*PAWN_VALUE, 'r': -1*ROOK_VALUE, 'n': -1*KNIGHT_VALUE, 'b': -1*BISHOP_VALUE, 'q': -1*QUEEN_VALUE}
+
+def get_board_score(board, is_white = True):
+    score = 0
+    for char in str(board):
+        value = letter_to_value_dict.get(char)
+        if value:
+            score += value
+    return score
